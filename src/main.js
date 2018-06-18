50176fa63783dabd2bfa7f4d6895d2b87102c5ea
Jess Robins <j.robins@thelevelup.com>
2018-06-17 22:29:10 -0400

    this does not work in safari

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueWait from 'vue-wait'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+// Vue.use(VueWait)
 
 new Vue({
   router,
